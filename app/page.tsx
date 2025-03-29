912c75c0f637a81fd9c7e1279121111e17b90aa2
Gaëtan H <gaetanhus@gmail.com>
2025-03-29 21:57:36 +0100

    add Card component and integrate Glitch for title rendering; update page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Glitch } from "@/components/glitch";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { useTranslations } from "next-intl";
 
 const Page = () => {
@@ -7,8 +9,22 @@ const Page = () => {
 
   return (
     <section className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">{t("Title")}</h1>
+      <h1 className="text-4xl font-bold">
+        <Glitch>{t("Title")}</Glitch>
+      </h1>
       <p className="text-lg">{t("Description")}</p>
+
+      {/* 2 defaults histories, and 1 card to open a writing prompt */}
+      <div className="flex flex-row">
+        <Card>
+          <CardHeader>
+            <CardTitle>{t("History")}</CardTitle>
+            <CardDescription>{t("HistoryDescription")}</CardDescription>
+          </CardHeader>
+          <CardContent className="text-center">{t("HistoryContent")}</CardContent>
+          <CardFooter className="justify-center">{t("HistoryFooter")}</CardFooter>
+        </Card>        
+      </div>
     </section>
   );
 }
