c48a5c0ff8746bf7728214e2da78acdd145f5fe5
Gaëtan H <gaetanhus@gmail.com>
2025-03-30 15:02:47 +0200

    refactor AuthButton component; enhance props handling and improve button accessibility

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { Component } from "@/lib/types";
-import { Button } from "./ui/button";
+import { Button, ButtonProps } from "./ui/button";
 import { Loader2, LogOut, User } from "lucide-react";
 import { redirect } from "next/navigation";
 import { clientEnv } from "@/lib/env/env.client";
-import { cn } from "@/lib/utils";
 import { useSession } from "@/lib/hooks/use-session";
 import { useTranslations } from "next-intl";
 
-export const AuthButton: Component<{ Navbar: boolean }> = ({ Navbar }) => {  
+type AuthButtonProps = ButtonProps & {
+  Navbar?: boolean;
+}
+
+export const AuthButton: Component<AuthButtonProps> = ({ Navbar, ...props }) => {
   const { user, signIn, loading, signOut } = useSession();
   const t = useTranslations("Navbar.SignIn");
 
@@ -33,7 +36,7 @@ export const AuthButton: Component<{ Navbar: boolean }> = ({ Navbar }) => {
 
   if (loading) {
     return (
-      <Button variant={Navbar ? "navbar" : "default"} disabled>
+      <Button variant={Navbar ? "navbar" : "default"} className="cursor-not-allowed" {...props} disabled>
         <Loader2 className="animate-spin" size={16} />
       </Button>
     )
@@ -41,7 +44,7 @@ export const AuthButton: Component<{ Navbar: boolean }> = ({ Navbar }) => {
 
   if (!user) {
     return (
-      <Button variant={Navbar ? "navbar" : "default"} onClick={() => signIn.discord()}>
+      <Button variant={Navbar ? "navbar" : "default"} onClick={() => signIn.discord()} {...props}>
         {getText("in")}
       </Button>
     )
@@ -56,7 +59,7 @@ export const AuthButton: Component<{ Navbar: boolean }> = ({ Navbar }) => {
       }
 
       redirect(clientEnv.NEXT_PUBLIC_BASE_URL)
-    }}>
+    }} {...props}>
       {getText("out")}
     </Button>
   )
