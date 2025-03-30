cb33d912e24b07facff13a42d9fb6ec1f0fa737d
Gaëtan H <gaetanhus@gmail.com>
2025-03-30 21:52:06 +0200

    feat: implement game continuation feature with card display and internationalization support

--- app/(game)/continue/page.tsx
+++ app/(game)/continue/page.tsx
@@ -1,14 +1,18 @@
 import { PageLayout } from "@/app/_l";
 import { Glitch } from "@/components/glitch";
-import Aurora from "@/components/ui/aurora";
 import { buttonVariants } from "@/components/ui/button";
-import { supabase } from "@/lib/supabase/client";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { createClient } from "@/lib/supabase/server";
+import { cn } from "@/lib/utils";
 import { getTranslations } from "next-intl/server";
+import Image from "next/image";
 import Link from "next/link";
 import { unauthorized } from "next/navigation";
 
 const Page = async() => {
   const t = await getTranslations("Pages.Continue");
+
+  const supabase = await createClient();
 
   const { data: session } = await supabase.auth.getSession();
   if (!session) return unauthorized();
@@ -39,6 +43,33 @@ const Page = async() => {
         </h1>
         <p className="text-lg">{t("Description")}</p>
       </section>
+
+      <div className="flex flex-col items-center mt-4">
+        {data.map((game, index) => (
+          <Card
+            className={cn("w-[350px] relative z-[100]", {
+              "pt-0": game.currentImageUrl
+            })}
+            key={index}
+          >
+            {game.currentImageUrl && (
+              <Image
+                src={game.currentImageUrl} alt={game.currentImageUrl}
+                width={500} height={300}
+                className="rounded-lg h-[200px] object-cover"
+              />
+            )}
+
+            <CardContent className="line-clamp-3">{game.currentScene}</CardContent>
+
+            <CardFooter>
+              <Link className={buttonVariants({ variant: "default" })} href={`/game/${game.id}`}>
+                {t("Continue")}
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </PageLayout>
   );
 }
