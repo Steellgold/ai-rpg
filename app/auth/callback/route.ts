5673eab34a5321aab2125a8b6e0e240ffae4a88d
Gaëtan H <gaetanhus@gmail.com>
2025-03-30 18:55:40 +0200

    refactor auth callback; replace createClient with supabase import

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@/lib/supabase/server";
+import { supabase } from "@/lib/supabase/client";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -8,7 +8,6 @@ export async function GET(request: Request) {
   const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
 
   if (code) {
-    const supabase = await createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
