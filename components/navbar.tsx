5f14eb1d0aed6a88dea5849beb111b642bfb8b08
Gaëtan H <gaetanhus@gmail.com>
2025-03-30 13:27:54 +0200

    refactor Navbar component; integrate translation for sign-in button and adjust layout positioning

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,20 +5,20 @@ import { ThemeLanguageSwitcher } from "./theme-and-language";
 import { Button } from "./ui/button";
 import { User } from "lucide-react";
 import { useSession } from "@/lib/hooks/use-session";
+import { useTranslations } from "next-intl";
 
 export const Navbar = (): ReactElement => {
   const { error, isLoading, user } = useSession();
+  const t = useTranslations("Navbar");
 
   return (
-    <nav className="absolute top-4 right-4 z-50 flex flex-row justify-between items-center p-2 text-white">
-      <div className="flex items-center space-x-4">
-        <ThemeLanguageSwitcher />
+    <nav className="absolute top-3 right-3 z-50 flex flex-row justify-between items-center p-2 flex items-center space-x-4">
+      <ThemeLanguageSwitcher />
         
-        <Button onClick={() => console.log("Sign in clicked")} className="dark:backdrop-blur-sm dark:bg-white/5 dark:text-white dark:hover:bg-white/10 transition dark:duration-200 dark:ease-in-out">
-          <User size={16} />
-          Se connecter
-        </Button>
-      </div>
+      <Button onClick={() => console.log("Sign in clicked")} className="dark:backdrop-blur-sm dark:bg-white/5 dark:text-white dark:hover:bg-white/10 transition dark:duration-200 dark:ease-in-out">
+        <User size={16} />
+        {t("SignIn.Label")}
+      </Button>
     </nav>
   )
 }
