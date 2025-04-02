5842462574d8b25178174fddd8748d410195ab3d
Gaëtan H <gaetanhus@gmail.com>
2025-04-02 19:13:51 +0200

    feat: add SUPABASE_SERVICE_ROLE_KEY to server environment configuration

--- lib/env/env.server.ts
+++ lib/env/env.server.ts
@@ -4,6 +4,7 @@ import { clientEnv } from "./env.client"
 const serverEnvSchema = z.object({
   ...Object.keys(clientEnv).reduce((acc, key) => ({ ...acc, [key]: z.string() }), {}),
   DATABASE_URL: z.string().url(),
+  SUPABASE_SERVICE_ROLE_KEY: z.string(),
   DIRECT_URL: z.string().url(),
 })
 
@@ -11,6 +12,7 @@ export const serverEnv = {
   ...clientEnv,
   DATABASE_URL: process.env.DATABASE_URL || "",
   DIRECT_URL: process.env.DIRECT_URL || "",
+  SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY || ""
 }
 
 if (typeof window === "undefined") {
