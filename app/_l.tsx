1a1d7636d608a1177bbf51d3679693ef66599f11
Gaëtan H <gaetanhus@gmail.com>
2025-04-02 21:24:19 +0200

    fix: adjust heading and paragraph sizes for better responsiveness in page.client.tsx and update layout spacing in _l.tsx

--- app/_l.tsx
+++ app/_l.tsx
@@ -9,11 +9,13 @@ type PropsWithChildren = DefaultPropsWithChildren & {
 export const PageLayout: Component<PropsWithChildren> = ({ children, aurora = ["#1a254f", "#1b274d", "#233161", "#16264d", "#2f9dd8"] }) => {
   return (
     <main className="relative overflow-hidden">
-      <div className="h-[300px]">
+      <div className="h-[150px]">
         <Aurora colorStops={aurora} blend={0.5} amplitude={1.5} speed={0.5} />
       </div>
 
       {children}
+
+      <div className="mt-12 h-[60px]" />
     </main>
   );
 }
