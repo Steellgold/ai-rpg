784ea3b682d861245f9c454d609ff9647e90bbd2
Gaëtan H <gaetanhus@gmail.com>
2025-04-05 14:09:40 +0200

    fix(page): comment out unused hr element and related section in Page component

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ const Page = async () => {
         <AiTextarea isPremium={user_data?.premium} />
       </section>
 
-      <hr className="my-56 border-t border-[#161616]" />
+      {/* <hr className="my-56 border-t border-[#161616]" />
 
       <section className="flex flex-col items-center w-full max-w-6xl mx-auto">
         <h2 className="flex flex-row gap-2 text-3xl items-center">
@@ -82,7 +82,7 @@ const Page = async () => {
             </div>
           ))}
         </div>
-      </section>
+      </section> */}
     </PageLayout>
   );
 }
