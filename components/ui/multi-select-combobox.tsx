5e85fdead05dfbff1740a0dd24626d64928addb2
Gaëtan H <gaetanhus@gmail.com>
2025-04-06 23:14:02 +0200

    fix(MultiSelectCombobox): update button text color for better visibility when disabled

--- components/ui/multi-select-combobox.tsx
+++ components/ui/multi-select-combobox.tsx
@@ -47,7 +47,7 @@ export function MultiSelectCombobox({
           size={selected.length > 0 ? "default" : "icon"}
           aria-expanded={open}
           disabled={disabled}
-          className={cn(disabled && "opacity-70 cursor-not-allowed", "h-8 rounded-full text-gray-400 hover:text-gray-300 !border border", {
+          className={cn(disabled && "opacity-70 cursor-not-allowed", "h-8 rounded-full text-white  !border border", {
             "!w-8": selected.length === 0
           })}
         >
