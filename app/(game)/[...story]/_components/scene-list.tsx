49a4fecc3219c6d3a00e149e553f8f8305d8c7ec
Gaëtan H <gaetanhus@gmail.com>
2025-04-06 20:59:05 +0200

    refactor(scene-list): simplify scene list component and remove unused props

--- app/(game)/[...story]/_components/scene-list.tsx
+++ app/(game)/[...story]/_components/scene-list.tsx
@@ -1,53 +1,73 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Check, ChevronRight, PanelRightClose, PanelRightOpen } from "lucide-react";
+import { Check, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Component } from "@/lib/types";
+import { Prisma } from "@prisma/client";
 
-interface SceneListProps {
-  scenes: any[];
+type SceneListProps = {
+  scenes: Prisma.SceneGetPayload<{
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      imageUrl: true,
+      imagePrompt: true,
+      choices: {
+        select: {
+          consequence: true,
+          text: true,
+          id: true,
+          isCustomChoice: true,
+          isPersonalized: true,
+          isItemRelated: true,
+          description: true
+        }
+      },
+      selected_choice_id: true,
+    }
+  }>[];
   storyId: string;
-  showPreviousScenes: boolean;
-  toggle: () => void;
+  selectedSceneId: string;
 }
 
 const SceneList: Component<SceneListProps> = ({
-  scenes, storyId, showPreviousScenes, toggle
+  scenes, storyId, selectedSceneId
 }) => {
-  if (!scenes || scenes.length <= 1) {
-    return <></>;
-  }
+  if (!scenes || scenes.length <= 1) return <></>;
 
   return (
-    <div className={cn({ "xl:w-1/5": showPreviousScenes })}>
-      {showPreviousScenes ? (
-        <Card className="w-full bg-gray-100/5">
-          <CardHeader className="pb-0 -mb-2">
-            <div className="flex justify-between items-center">
-              <CardTitle>Scènes</CardTitle>
-              <Button variant="outline" size="icon" className="bg-white/10 hover:bg-white/20" onClick={toggle}>
-                <PanelRightClose size={16} />
-              </Button>
-            </div>
-          </CardHeader>
-          <CardContent className="flex flex-row xl:flex-col gap-0.5 xl:gap-2 items-center flex-wrap">
-            {scenes.map((scene) => (
-              <>
-                <Link key={scene.id} href={`/${storyId}/${scene.id}`} className="buttonVariants">
-                  {scene.title}
-                  {scene.selected_choice_id && <Check className="ml-2" size={16} color="green" />}
-                </Link>
-                {scene.id !== scenes[scenes.length - 1].id && <ChevronRight className="block xl:hidden" size={16} color="gray" />}
-              </>
-            ))}
-          </CardContent>
-        </Card>
-      ) : (
-        <Button variant="outline" size="icon" className="bg-white/10 hover:bg-white/20" onClick={toggle}>
-          <PanelRightOpen size={16} />
-        </Button>
-      )}
+    <div className="w-1/5">
+      <Card className="w-full bg-gray-100/5">
+        <CardHeader className="pb-0 -mb-2">
+          <CardTitle>Scènes</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-row xl:flex-col gap-0.5 xl:gap-2 items-center flex-wrap">
+          {scenes.map((scene) => (
+            <Link
+              key={scene.id}
+              href={
+                scene.selected_choice_id ? `/${storyId}/${scene.id}` : `/${storyId}`
+              }
+              className={cn(
+                "flex flex-row gap-2 items-center w-full p-2 rounded-md hover:bg-gray-100/10",
+                selectedSceneId === scene.id ? "bg-gray-100/10" : "bg-transparent"
+              )}
+            >
+              <div className="flex flex-row gap-2 items-center w-full">
+                <div className="flex flex-col w-full">
+                  <span className="text-sm font-medium">{scene.title}</span>
+                </div>
+                {scene.selected_choice_id && (
+                  <Check className="text-green-500" size={16} />
+                )}
+              </div>
+              <ChevronRight size={16} className="text-gray-400" />
+            </Link>
+          ))}
+        </CardContent>
+      </Card>
     </div>
   );
 };
