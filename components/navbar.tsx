4c921a3e9a6d05fb99d0c6aac2574e4edc7d86e0
Gaëtan H <gaetanhus@gmail.com>
2025-04-08 00:35:57 +0200

    feat(credits): add CreditsButton component to display user credits in the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,13 @@
 import { ReactElement } from "react";
 import { AuthButton } from "./auth-button";
 import { LanguageSelector } from "./language-selector";
-import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { CreditsButton } from "./credits-button";
 
 export const Navbar = (): ReactElement => {
   return (
     <div className="fixed top-0 left-0 right-0 shadow-md z-50">
       <nav className="flex items-center justify-end p-4 gap-2">
-        <Link href={"/continue"} className={buttonVariants({ variant: "ghost" })}>
-          Stories
-        </Link>
+        <CreditsButton />
         <LanguageSelector />
         <AuthButton Navbar />
       </nav>
