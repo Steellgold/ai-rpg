03f4587d1c67f811582bb87070f8c5e6fbd10634
Gaëtan H <gaetanhus@gmail.com>
2025-04-09 00:38:13 +0200

    fix: update import path for ProfileComponent to reflect new directory structure

--- components/naviguation/navbar.tsx
+++ components/naviguation/navbar.tsx
@@ -2,12 +2,12 @@
 
 import { useState, useEffect } from "react"
 import Link from "next/link"
-import { BookDashed, Home, LibraryBig, Menu, Settings } from "lucide-react"
+import { Home, LibraryBig, Menu, Settings } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { cn } from "@/lib/utils"
-import { ProfileComponent } from "../auth-related/profile-component"
+import { ProfileComponent } from "@/components/auth/profile-component"
 import Image from "next/image"
 
 const navigationItems = [
