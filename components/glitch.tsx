68249138848e8e19a1304a68f3ba5dde1964a87f
Gaëtan H <me@gaetanhus.fr>
2025-04-08 16:21:08 +0200

    fix: add missing semicolon in Glitch component

--- components/glitch.tsx
+++ components/glitch.tsx
@@ -2,7 +2,7 @@ import { Component } from "@/lib/types"
 import { Rubik_Glitch } from "next/font/google"
 import { PropsWithChildren } from "react"
 
-const rubikGlitch = Rubik_Glitch({ weight: "400", subsets: ["latin"] })
+const rubikGlitch = Rubik_Glitch({ weight: "400", subsets: ["latin"] });
 
 export const Glitch: Component<PropsWithChildren> = ({ children }) => {
   return (
