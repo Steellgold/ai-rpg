17b9fd8b4ff9cf2a1de846acb70780602e172fe0
Gaëtan H <gaetanhus@gmail.com>
2025-04-09 18:51:34 +0200

    feat(textarea): integrate dragon SVG image for enhanced visual appeal

--- components/story/textarea.tsx
+++ components/story/textarea.tsx
@@ -22,6 +22,7 @@ import { useToast } from "@/lib/hooks/use-toast";
 import Link from "next/link";
 import { suggestions } from "@/lib/suggestions";
 import { useDraft } from "@/lib/hooks/use-draft";
+import Image from "next/image";
 
 export const AiTextarea: Component<HTMLAttributes<HTMLDivElement>> = ({ className }): ReactElement => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -133,189 +134,202 @@ export const AiTextarea: Component<HTMLAttributes<HTMLDivElement>> = ({ classNam
   return (
     <div className="flex flex-col items-center w-full gap-4">
       <CustomScrollbar />
-      <div className={cn("relative border-2 rounded-xl overflow-hidden w-full", className, {
-        "bg-[#070910] border border-[#173a8940]": true
-      })}>
-        <ShineBorder className="rounded-xl" shineColor={["#2744ad", "#6c83d6", "#0d288a"]} />
-        <textarea
-          ref={textareaRef}
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          disabled={isGenerating}
-          placeholder={
-            "The story takes place in the kingdom of Kiyomitsugawa, a prosperous nation bathed by sacred rivers, with landscapes adorned with thousand-year-old cherry trees and majestic mountains. This kingdom, imbued with ser...."
-          }
-          className={cn(
-            "w-full resize-none py-4 px-4 outline-none bg-transparent text-gray-200 placeholder:text-gray-500 min-h-[110px] custom-scrollbar", {
-              "animate-pulse italic text-gray-400": isGenerating
+      <div className="relative w-full">
+        <div className={cn("relative border-2 rounded-xl overflow-hidden w-full", className, {
+          "bg-[#070910] border border-[#173a8940]": true
+        })}>
+          <ShineBorder className="rounded-xl" shineColor={["#2744ad", "#6c83d6", "#0d288a"]} />
+          <textarea
+            ref={textareaRef}
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            disabled={isGenerating}
+            placeholder={
+              "The story takes place in the kingdom of Kiyomitsugawa, a prosperous nation bathed by sacred rivers, with landscapes adorned with thousand-year-old cherry trees and majestic mountains. This kingdom, imbued with ser...."
             }
-          )}
-        />
-        
-        <div className="flex items-center justify-between px-2">
-          <div className="flex items-center flex-wrap gap-1.5">
-            <Button
-              size={"default"}
-              variant="ghost"
-              className="!border rounded-full transition-opacity h-8 px-3 cursor-pointer"
-              disabled={isGenerating}
-              asChild
-            >
-              <Link href={"/editor"}>
-                {t("AiTextarea.OpenEditor")}
-                <Maximize className="h-4 w-4 text-gray-200" />
-              </Link>
-            </Button>
+            className={cn(
+              "w-full resize-none py-4 px-4 outline-none bg-transparent text-gray-200 placeholder:text-gray-500 min-h-[110px] custom-scrollbar", {
+                "animate-pulse italic text-gray-400": isGenerating
+              }
+            )}
+          />
+          
+          <div className="flex items-center justify-between px-2">
+            <div className="flex items-center flex-wrap gap-1.5">
+              <Button
+                size={"default"}
+                variant="ghost"
+                className="!border rounded-full transition-opacity h-8 px-3 cursor-pointer"
+                disabled={isGenerating}
+                asChild
+              >
+                <Link href={"/editor"}>
+                  {t("AiTextarea.OpenEditor")}
+                  <Maximize className="h-4 w-4 text-gray-200" />
+                </Link>
+              </Button>
+            </div>
+
+            <p className={cn("text-sm", {
+              "text-red-400": prompt.length >= 2500,
+              "text-gray-400": prompt.length < 2500
+            })}>
+              {prompt.length} / 2500
+            </p>
           </div>
 
-          <p className={cn("text-sm", {
-            "text-red-400": prompt.length >= 2500,
-            "text-gray-400": prompt.length < 2500
-          })}>
-            {prompt.length} / 2500
-          </p>
-        </div>
+          <div className="flex items-center justify-between p-2">
+            <div className="flex items-center flex-wrap gap-1.5">
+              {(selectedGenres.length > 0 || prompt.length > 0) && (!isGenerating || isInputValid) && (
+                <Button
+                  size={"toolIcon"}
+                  variant="ghost"
+                  className="!border border-red-400/20 rounded-full transition-opacity hover:bg-red-400/10 cursor-pointer"
+                  onClick={handleClear}
+                  disabled={isGenerating}
+                >
+                  <X className="h-4 w-4 text-red-400" />
+                </Button>
+              )}
 
-        <div className="flex items-center justify-between p-2">
-          <div className="flex items-center flex-wrap gap-1.5">
-            {(selectedGenres.length > 0 || prompt.length > 0) && (!isGenerating || isInputValid) && (
+              <MultiSelectCombobox
+                options={genreIds.map((genre) => ({
+                  label: `${u("Genres." + genre + ".label")}`,
+                  description: `${u("Genres." + genre + ".description")}`,
+                  value: genre,
+                }))}
+                selected={selectedGenres}
+                onChange={setSelectedGenres}
+                placeholder={t("AiTextarea.SelectGenres")}
+                emptyMessage={t("AiTextarea.NoGenresFound")}
+                disabled={isGenerating}
+              />
+              
+              <StoryLanguageSelector
+                selectedLanguage={storyLanguage}
+                onLanguageChange={setStoryLanguage}
+              />
+
               <Button
                 size={"toolIcon"}
                 variant="ghost"
-                className="!border border-red-400/20 rounded-full transition-opacity hover:bg-red-400/10 cursor-pointer"
-                onClick={handleClear}
+                className={cn(
+                  "!border rounded-full transition-opacity cursor-pointer", {
+                    "border-teal-400/20 hover:bg-teal-400/10": childMode,
+                    "border-gray-400/20 hover:bg-gray-400/10": !childMode
+                  }
+                )}
+                onClick={() => setChildMode(!childMode)}
                 disabled={isGenerating}
               >
-                <X className="h-4 w-4 text-red-400" />
+                <Baby className={cn("h-4 w-4", { "text-teal-400": childMode, "text-gray-200": !childMode })} />
               </Button>
-            )}
 
-            <MultiSelectCombobox
-              options={genreIds.map((genre) => ({
-                label: `${u("Genres." + genre + ".label")}`,
-                description: `${u("Genres." + genre + ".description")}`,
-                value: genre,
-              }))}
-              selected={selectedGenres}
-              onChange={setSelectedGenres}
-              placeholder={t("AiTextarea.SelectGenres")}
-              emptyMessage={t("AiTextarea.NoGenresFound")}
-              disabled={isGenerating}
-            />
-            
-            <StoryLanguageSelector
-              selectedLanguage={storyLanguage}
-              onLanguageChange={setStoryLanguage}
-            />
-
-            <Button
-              size={"toolIcon"}
-              variant="ghost"
-              className={cn(
-                "!border rounded-full transition-opacity cursor-pointer", {
-                  "border-teal-400/20 hover:bg-teal-400/10": childMode,
-                  "border-gray-400/20 hover:bg-gray-400/10": !childMode
-                }
-              )}
-              onClick={() => setChildMode(!childMode)}
-              disabled={isGenerating}
-            >
-              <Baby className={cn("h-4 w-4", { "text-teal-400": childMode, "text-gray-200": !childMode })} />
-            </Button>
-
-            <Button
-              size={"toolIcon"}
-              variant="ghost"
-              className={cn(
-                "!border rounded-full transition-opacity cursor-pointer", {
-                  "w-8": true,
-                  "border-teal-400/20 hover:bg-teal-400/10": items,
-                  "border-gray-400/20 hover:bg-gray-400/10": !items
-                }
-              )}
-              onClick={() => setItems(!items)}
-              disabled={isGenerating || isLoggingIn}
-            >
-              <Pickaxe className={cn("h-4 w-4", {
-                "text-teal-400": items,
-                "text-gray-200": !items
-              })} />
-            </Button>
-
-            <Button
-              size={"default"}
-              variant="ghost"
-              className={cn(
-                "!border rounded-full transition-opacity !h-8 cursor-pointer px-2.5", {
-                  "border-teal-400/20 hover:bg-teal-400/10": publicMode,
-                  "border-gray-400/20 hover:bg-gray-400/10": !publicMode,
-                  "border-red-400/20 hover:bg-red-400/10": !publicMode,
-                }
-              )}
-              onClick={() => setPublicMode(!publicMode)}
-              disabled={isGenerating || isLoggingIn}
-            >
-              {cloneElement(
-                publicMode
-                  ? <LockOpen className="h-4 w-4" />
-                  : <Lock className="h-4 w-4" />, {
-                    className: cn("h-4 w-4", {
-                      "text-teal-400": publicMode,
-                      "text-red-200": !publicMode,
-                      "text-gray-200": !publicMode
-                    })
-              })}
-
-              <span className={cn("text-sm", {
-                "text-teal-400": publicMode,
-                "text-red-200": !publicMode,
-                "text-gray-200": !publicMode
-              })}>
-                {publicMode ? t("AiTextarea.PublicModeOn") : t("AiTextarea.PublicModeOff")}
-              </span>
-            </Button>
-          </div>
-
-          <div className="flex items-center">
-            {user ? (
-              <>
-                <EnhancedSendButton
-                  isGenerating={isGenerating}
-                  isInputValid={isInputValid}
-                  handleSend={handleSend}
-                  activeFeatures={activeFeatures}
-                  promptLength={prompt.length}
-                  generateText={t("AiTextarea.Generate")}
-                  calculateCreditCost={calculateCreditCost}
-                  creditHave={
-                    isCreditsFetching ? 0 : credits
+              <Button
+                size={"toolIcon"}
+                variant="ghost"
+                className={cn(
+                  "!border rounded-full transition-opacity cursor-pointer", {
+                    "w-8": true,
+                    "border-teal-400/20 hover:bg-teal-400/10": items,
+                    "border-gray-400/20 hover:bg-gray-400/10": !items
                   }
-                />
-              </>
-            ) : (
-              <>
-                <Button
-                  onClick={() => signIn.discord(prompt, selectedGenres)}
-                  className={cn(
-                    "!h-8 !px-4 rounded-full transition-opacity",
-                    isLoggingIn ? "opacity-50 cursor-not-allowed" : "opacity-100", {
-                      "bg-blue-600 hover:bg-blue-600": !isLoggingIn && !childMode,
-                      "cursor-not-allowed bg-blue-600/50 hover:bg-blue-600/50": isLoggingIn && !childMode,
-                      "bg-teal-600 hover:bg-teal-500": !isLoggingIn && childMode,
-                      "cursor-not-allowed bg-teal-600/50 hover:bg-teal-600/50": isLoggingIn && childMode
+                )}
+                onClick={() => setItems(!items)}
+                disabled={isGenerating || isLoggingIn}
+              >
+                <Pickaxe className={cn("h-4 w-4", {
+                  "text-teal-400": items,
+                  "text-gray-200": !items
+                })} />
+              </Button>
+
+              <Button
+                size={"default"}
+                variant="ghost"
+                className={cn(
+                  "!border rounded-full transition-opacity !h-8 cursor-pointer px-2.5", {
+                    "border-teal-400/20 hover:bg-teal-400/10": publicMode,
+                    "border-gray-400/20 hover:bg-gray-400/10": !publicMode,
+                    "border-red-400/20 hover:bg-red-400/10": !publicMode,
+                  }
+                )}
+                onClick={() => setPublicMode(!publicMode)}
+                disabled={isGenerating || isLoggingIn}
+              >
+                {cloneElement(
+                  publicMode
+                    ? <LockOpen className="h-4 w-4" />
+                    : <Lock className="h-4 w-4" />, {
+                      className: cn("h-4 w-4", {
+                        "text-teal-400": publicMode,
+                        "text-red-200": !publicMode,
+                        "text-gray-200": !publicMode
+                      })
+                })}
+
+                <span className={cn("text-sm", {
+                  "text-teal-400": publicMode,
+                  "text-red-200": !publicMode,
+                  "text-gray-200": !publicMode
+                })}>
+                  {publicMode ? t("AiTextarea.PublicModeOn") : t("AiTextarea.PublicModeOff")}
+                </span>
+              </Button>
+            </div>
+
+            <div className="flex items-center">
+              {user ? (
+                <>
+                  <EnhancedSendButton
+                    isGenerating={isGenerating}
+                    isInputValid={isInputValid}
+                    handleSend={handleSend}
+                    activeFeatures={activeFeatures}
+                    promptLength={prompt.length}
+                    generateText={t("AiTextarea.Generate")}
+                    calculateCreditCost={calculateCreditCost}
+                    creditHave={
+                      isCreditsFetching ? 0 : credits
                     }
-                  )}
-                >
-                  {isLoggingIn
-                    ? <Loader className="animate-spin h-4 w-4 text-gray-400" />
-                    : <User className="h-4 w-4 text-gray-200" />
-                  }
-                  <span className="text-white">{t("AiTextarea.SignIn")}</span>
-                </Button>
-              </>
-            )}
+                  />
+                </>
+              ) : (
+                <>
+                  <Button
+                    onClick={() => signIn.discord(prompt, selectedGenres)}
+                    className={cn(
+                      "!h-8 !px-4 rounded-full transition-opacity",
+                      isLoggingIn ? "opacity-50 cursor-not-allowed" : "opacity-100", {
+                        "bg-blue-600 hover:bg-blue-600": !isLoggingIn && !childMode,
+                        "cursor-not-allowed bg-blue-600/50 hover:bg-blue-600/50": isLoggingIn && !childMode,
+                        "bg-teal-600 hover:bg-teal-500": !isLoggingIn && childMode,
+                        "cursor-not-allowed bg-teal-600/50 hover:bg-teal-600/50": isLoggingIn && childMode
+                      }
+                    )}
+                  >
+                    {isLoggingIn
+                      ? <Loader className="animate-spin h-4 w-4 text-gray-400" />
+                      : <User className="h-4 w-4 text-gray-200" />
+                    }
+                    <span className="text-white">{t("AiTextarea.SignIn")}</span>
+                  </Button>
+                </>
+              )}
+            </div>
           </div>
         </div>
+
+        <Image
+          src={"/dragon.svg"}
+          alt="dragon"
+          width={100}
+          height={100}
+          className={cn(
+            "absolute -top-14 -right-[45px] rotate-12 select-none selection-none pointer-events-none",
+            "hidden lg:block"
+          )}
+        />
       </div>
 
       <div>
