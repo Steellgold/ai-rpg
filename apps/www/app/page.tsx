ec7de167f035f4304cc5c86a08c7b7a207a6a1b3
Gaëtan H <gaetanhus@gmail.com>
2025-04-12 13:23:02 +0200

    fix: adjust font weight and layout for improved readability in Page component

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -4,7 +4,7 @@ import { AiTextarea } from "@/components/story/textarea";
 import { Syne } from "next/font/google";
 import { getTranslations } from "next-intl/server";
 
-const syne = Syne({ subsets: ["latin"], weight: ["800", "500"] });
+const syne = Syne({ subsets: ["latin"] });
 
 const Page = async(): Promise<ReactElement>=> {
   const t = await getTranslations("Home");
@@ -13,13 +13,15 @@ const Page = async(): Promise<ReactElement>=> {
     <PageLayout>
       <section className="flex flex-col items-center max-w-2xl mx-auto mt-10">
         <div className="flex flex-col items-center text-center">
-          <h1 className={`${syne.className} text-5xl font-extrabold`}>{t("Title")}</h1>
-          <p className={`${syne.className} text-lg font-medium`}>{t("Subtitle")}</p>
+          <h1 className={`${syne.className} text-3xl lg:text-5xl font-extrabold`}>{t("Title")}</h1>
+          <p className={`${syne.className} text-sm lg:text-lg font-medium`}>{t("Subtitle")}</p>
         </div>
 
-        <div className="h-8" />
+        <div className="h-6" />
 
-        <AiTextarea />
+        <div className="px-4 lg:px-0 w-full">
+          <AiTextarea />
+        </div>
       </section>
     </PageLayout>
   );
