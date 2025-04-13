69fe80ea701aad0311ccb8cdad173580d7d1ae7a
Gaëtan H <gaetanhus@gmail.com>
2025-04-13 12:46:21 +0200

    refactor: improve JSX structure in Page component for better readability

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -10,20 +10,22 @@ const Page = async(): Promise<ReactElement>=> {
   const t = await getTranslations("Home");
 
   return (
-    <PageLayout>
-      <section className="flex flex-col items-center max-w-2xl mx-auto mt-10">
-        <div className="flex flex-col items-center text-center">
-          <h1 className={`${syne.className} text-3xl lg:text-5xl font-extrabold`}>{t("Title")}</h1>
-          <p className={`${syne.className} text-sm lg:text-lg font-medium`}>{t("Subtitle")}</p>
-        </div>
+    <>
+      <PageLayout>
+        <section className="flex flex-col items-center max-w-2xl mx-auto mt-10">
+          <div className="flex flex-col items-center text-center">
+            <h1 className={`${syne.className} text-3xl lg:text-5xl font-extrabold`}>{t("Title")}</h1>
+            <p className={`${syne.className} text-sm lg:text-lg font-medium`}>{t("Subtitle")}</p>
+          </div>
 
-        <div className="h-6" />
+          <div className="h-6" />
 
-        <div className="px-4 lg:px-0 w-full">
-          <AiTextarea />
-        </div>
-      </section>
-    </PageLayout>
+          <div className="px-4 lg:px-0 w-full">
+            <AiTextarea />
+          </div>
+        </section>
+      </PageLayout>
+    </>
   );
 }
 
