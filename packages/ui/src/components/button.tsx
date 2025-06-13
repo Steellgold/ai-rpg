57025f7832e574a5637ebabb00bd6e7bb6969d95
Gaëtan H <gaetanhus@gmail.com>
2025-06-13 20:25:07 +0200

    feat: add 'ycon' size variant to Button component for enhanced styling options

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -30,6 +30,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        ycon: "h-9 w-9 rounded-full has-[>svg]:px-0",
         icon: "size-9",
       },
     },
