a8545e6e8cf6735b5e53d65d56df0c53fa644af5
Gaëtan H <gaetanhus@gmail.com>
2025-06-13 14:46:18 +0200

    feat: refactor RootLayout to use Component type and add metadata typing

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,11 @@
+import { metadata as defaultMetadata } from "@/lib/metadata"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
+import { Component } from "@/lib/component"
+import { PropsWithChildren } from "react"
+import { Metadata } from "next"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -13,18 +17,16 @@ const fontMono = Geist_Mono({
   variable: "--font-mono",
 })
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export const metadata: Metadata = { ...defaultMetadata };
+
+const RootLayout: Component<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
+      <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
         <Providers>{children}</Providers>
       </body>
     </html>
   )
 }
+
+export default RootLayout
